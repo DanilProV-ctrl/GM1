c56ed0520b1642e7cfe0143dc995d09008261be6
DanilProV-ctrl <ProVDanil2019@yandex.ru>
2019-12-08 13:35:53 +1000

    gmONE

---
+++ kkk.py
@@ -0,0 +1,5 @@
+a = ["shak"]
+a.append('x')
+a.insert(2,"b")
+print(len(a))
+#len(a) - pokaz dannogo spiska (spiska a)
--- main.py
+++ main.py
@@ -13,15 +13,45 @@ while i < 10:
     print(i)
     i += 1
     
+  
 
 import pygame as pg
 
-WIN_WIDTH,WIN_HEIGHT, = 780,780
+level = [
+    '--------------------------',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        - ',
+    '-                        - ',
+    '-                        - ',
+    '-                        - ',
+    '-                        - ',
+    '-                        - ',
+    '-                        - ',
+    '-                        - ',
+    '-                        - ',
+    '-                        - ',
+    '-                        - ',
+    '----------------------------'
+]
+
+WIN_WIDTH,WIN_HEIGHT, = 780, 630
 BG_COLOR = (192, 192, 192)
+BRICK_WIDTH = BRICK_HEIGHT = 30
+BRICK_COLOR = (10, 128, 100)
 
 pg.init()
 pg.display.set_caption('first game')
-screen = pg.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
+screen = pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+
+brick = pg.Surface((BRICK_WIDTH, BRICK_HEIGHT))
+brick.fill(BRICK_COLOR)
 
 run = True
 while run:
@@ -30,6 +60,19 @@ while run:
             run = False
 
     screen.fill(BG_COLOR)
+
+
+    x = 0
+    y = 0
+    for row in level:
+        for col in row:
+            if col == "-":
+                screen.blit(brick, (x, y))
+            x += BRICK_WIDTH
+        y += BRICK_HEIGHT
+        x = 0
+
+    
     pg.display.update()
 
 
