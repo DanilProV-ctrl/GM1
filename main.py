950d57a7366c31c04f08f24635faf3ef3a3f1455
DanilProV-ctrl <ProVDanil2019@yandex.ru>
2020-03-15 13:36:11 +1000

    Final Update.

--- main.py
+++ main.py
@@ -5,7 +5,13 @@
 # for - medlenny cikl,konechnyi
 # while - beskonechnyi,cikl s usloviemt
 # // napomny
-# 
+#
+import pygame as pg
+import sounddevice as sd
+from pygame import gfxdraw
+import numpy
+
+
 from time import sleep
 i = 0
 while i < 11:
@@ -13,9 +19,6 @@ while i < 11:
     print(i)
     i += 1
 
-
-import pygame as pg
-from pygame import gfxdraw
 
 level = [
     '----------------------------------------------------------------------------------------------------------------------------------------------------------------',
@@ -87,12 +90,7 @@ def face(color):
 
 player_rect = player.get_rect(center=(WIN_WIDTH // 2, WIN_HEIGHT // 2))
 txt = pg.font.SysFont('Arial', 22, True, True)
-text_xy = ((WIN_WIDTH - txt.size(f'Штрафных очков = {penalty}')[0] - 300, 3),
-
-)
-
-
-
+text_xy = ((WIN_WIDTH - txt.size(f'Штрафных очков = {penalty}')[0] - 300, 3),)
 
 
 button = pg.Surface((BTN_W, BTN_H))
@@ -101,76 +99,97 @@ txte = pg.font.SysFont('Arial', 22, True, False)
 text1 = 'Play again?'
 text1_pos = txte.size(text1)
 
+result = [WIN_HEIGHT // 2]
+yyy = [player_rect.y] * 100
+
+
+def audio_callback(indata, frames, time, status):
+    volume = numpy.linalg.norm(indata) * 20
+    yyy.append(volume)
+    yyy.pop(0)
+    result[0] = sum(yyy) / len(yyy)
+
+
 color = BLUE
 face(color)
 run = True
-while run:
-    for e in pg.event.get():
-        if e.type == pg.QUIT or e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE:
-            run = False
-        elif e.type == pg.MOUSEBUTTONDOWN:
-            if e.button == 1:
-                mouse_position = pg.mouse.get_pos()
-                if (mouse_position[0] >= 280
-                    and mouse_position[0] <= 500
-                    and mouse_position[1] >= WIN_HEIGHT // 2
-                    and mouse_position[1] <= WIN_HEIGHT // 2 + BTN_H):
+stream = sd.InputStream(callback=audio_callback)
+with stream:
+    while run:
+        for e in pg.event.get():
+            if e.type == pg.QUIT or e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE:
+                run = False
+            elif e.type == pg.MOUSEBUTTONDOWN:
+                if e.button == 1:
+                    mouse_position = pg.mouse.get_pos()
+                    if (mouse_position[0] >= 280
+                        and mouse_position[0] <= 500
+                        and mouse_position[1] >= WIN_HEIGHT // 2
+                        and mouse_position[1] <= WIN_HEIGHT // 2 + BTN_H):
                         print(101010)
+                        penalty = 0
+                        dx = 0
+                        player_rect.center = WIN_WIDTH // 2, WIN_HEIGHT // 2
+                        result = [WIN_HEIGHT // 2]
+                        yyy = [player_rect.y] * 100
+                        color = BLUE
+                        face(color)
 
-    keys = pg.key.get_pressed()
-    if keys[pg.K_RIGHT]:
-        player_rect.x += PL_SPEED
-    if keys[pg.K_LEFT]:
-        player_rect.x -= PL_SPEED
-    if keys[pg.K_UP]:
-        player_rect.y -= PL_SPEED
-    if keys[pg.K_DOWN]:
-        player_rect.y += PL_SPEED
-    if color == RED:
-        color = BLUE
-        face(color)
-
-    screen.fill(BG_COLOR)
-    if dx > -WIN_WIDTH * 4:
-        dx -= BG_SPEED
-    else:
-        if player_rect.x < WIN_WIDTH - PLAER_SIZE:
+        player_rect.y = WIN_HEIGHT - result[0]
+        '''
+        keys = pg.key.get_pressed()
+        if keys[pg.K_RIGHT]:
             player_rect.x += PL_SPEED
+        if keys[pg.K_LEFT]:
+            player_rect.x -= PL_SPEED
+        if keys[pg.K_UP]:
+            player_rect.y -= PL_SPEED
+        if keys[pg.K_DOWN]:
+            player_rect.y += PL_SPEED
+        '''
+        if color == RED:
+            color = BLUE
+            face(color)
 
+        screen.fill(BG_COLOR)
+        if dx > -WIN_WIDTH * 4:
+            dx -= BG_SPEED
+        else:
+            if player_rect.x < WIN_WIDTH - PLAER_SIZE:
+                player_rect.x += PL_SPEED
 
-    x = dx
-    y = 0
-    for row in level:
-        for col in row:
-            if col == "-":
-                # screen.blit(brick, (x, y))
-                brick = pg.draw.rect(screen, BRICK_COLOR, [x, y, BRICK_WIDTH, BRICK_HEIGHT])
-                pg.draw.rect(screen, BRICK_COLOR_2, [x, y, BRICK_WIDTH, BRICK_HEIGHT], 2)
-                if brick.colliderect(player_rect):
-                    if player_rect.x < WIN_WIDTH - PLAER_SIZE * 2:
-                        if color == BLUE:
-                            color = RED
-                            face(color)
-                        penalty += 0.1
-            x += BRICK_WIDTH
-        y += BRICK_HEIGHT
         x = dx
+        y = 0
+        for row in level:
+            for col in row:
+                if col == "-":
+                    # screen.blit(brick, (x, y))
+                    brick = pg.draw.rect(screen, BRICK_COLOR, [x, y, BRICK_WIDTH, BRICK_HEIGHT])
+                    pg.draw.rect(screen, BRICK_COLOR_2, [x, y, BRICK_WIDTH, BRICK_HEIGHT], 2)
+                    if brick.colliderect(player_rect):
+                        if player_rect.x < WIN_WIDTH - PLAER_SIZE * 2:
+                            if color == BLUE:
+                                color = RED
+                                face(color)
+                            penalty += 0.1
+                x += BRICK_WIDTH
+            y += BRICK_HEIGHT
+            x = dx
 
-
-    if player_rect.x < WIN_WIDTH - PLAER_SIZE:
-        screen.blit(player, player_rect)
-        pg.display.set_caption(f' FPS: {round(clock.get_fps(), 1)}')
-        screen.blit(
-            txt.render(f'penalty point = {round(penalty, 1)}', True, RED, None), text_xy
-        )
-    else:
-        screen.blit(button, ((WIN_WIDTH - BTN_W) // 2, WIN_HEIGHT // 2))
-        button.blit(
-            txt.render(text1, True, WHITE, None),
-            ((BTN_W - text1_pos[0]) // 2, (BTN_H - text1_pos[1]) // 2))
-        screen.blit(
-            txt.render(f'penalty point = {round(penalty, 1)}', True, RED, None),
-            ((WIN_WIDTH - txte.size(f'penalty point = {round(penalty, 1)}')[0]) // 2,
-             WIN_HEIGHT // 2 - BTN_H))
-    pg.display.update()
-    clock.tick(FPS)
+        if player_rect.x < WIN_WIDTH - PLAER_SIZE:
+            screen.blit(player, player_rect)
+            pg.display.set_caption(f' FPS: {round(clock.get_fps(), 1)}')
+            screen.blit(
+                txt.render(f'penalty point = {round(penalty, 1)}', True, RED, None), text_xy
+            )
+        else:
+            screen.blit(button, ((WIN_WIDTH - BTN_W) // 2, WIN_HEIGHT // 2))
+            button.blit(
+                txt.render(text1, True, WHITE, None),
+                ((BTN_W - text1_pos[0]) // 2, (BTN_H - text1_pos[1]) // 2))
+            screen.blit(
+                txt.render(f'penalty point = {round(penalty, 1)}', True, RED, None),
+                ((WIN_WIDTH - txte.size(f'penalty point = {round(penalty, 1)}')[0]) // 2,
+                 WIN_HEIGHT // 2 - BTN_H))
+        pg.display.update()
+        clock.tick(FPS)
